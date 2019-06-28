178bcc3481bc433dc5aeaf7442b6c18e91d16453
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-06-28 19:09:29 +0200

    Update functions-data.js

--- week-2/team/functions-data.js
+++ week-2/team/functions-data.js
@@ -1,8 +1,18 @@
 // data (variables)
-
-
+var team_name = "COLORS";
+var team_members = ["blue","green","pink","purple","yellow"];
 
 // functions
-
-
-/* onclick's */
+function print_team_name(teamName) {
+    console.log("Team name is "+teamName);
+}
+function print_team_members(teamMembers){
+    console.log("Team members are ");
+    for(let i=0; i<teamMembers.length ;i++){
+        console.log(teamMembers[i]);
+    }
+}
+function introduce(teamName, teamMembers){
+    print_team_name(teamName);
+    print_team_members(teamMembers);
+}
