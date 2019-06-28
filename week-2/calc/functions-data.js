4440096db3c6357fed9e49e4425f68457a9760b1
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-06-28 19:10:39 +0200

    Update functions-data.js

--- week-2/calc/functions-data.js
+++ week-2/calc/functions-data.js
@@ -1,8 +1,29 @@
 // data (variables)
-
+var num_1 = 10;
+var num_2 = 9;
 
 
 // functions
+function print_vals(a,b){
+    console.log(a,b);
+}
 
+function do_math(a,b,func){
+    console.log(func(a,b));
+}
 
-/* onclick's */
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
