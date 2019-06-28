307a896ad76b87866853d681fa83f4ff34caf842
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-06-28 19:08:01 +0200

    Update scripts.js

--- week-2/scripts.js
+++ week-2/scripts.js
@@ -1,5 +1,18 @@
 // data (variables)
-
+var project = { name:"Google", 
+                link:"www.google.com",
+                description: "Search motor" };
 
 
 // functions
+function print_deets(obj){
+    for(let det in obj){
+        console.log(obj[det]);
+    }
+}
+
+// onclicks
+var print_det = document.getElementById("print-details");
+print_det.onclick = function() { print_deets(project); } 
+                            
+                          
