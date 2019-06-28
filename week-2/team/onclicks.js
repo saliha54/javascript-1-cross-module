841229c9f7552d432a0ae65606fa598943aa554e
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-06-28 19:08:39 +0200

    Update onclicks.js

--- week-2/team/onclicks.js
+++ week-2/team/onclicks.js
@@ -1,1 +1,13 @@
-onclicks.js
+
+var printTeamName = document.getElementById("print-team-name");
+printTeamName.onclick = function() {print_team_name(team_name);}
+
+var printTeamMembers = document.getElementById("print-team-members");
+printTeamMembers.onclick = function() {
+                            print_team_members(team_members);
+                          }
+
+var printIntroduction = document.getElementById("print-introduction");
+printIntroduction.onclick = function() {
+                            introduce(team_name, team_members);
+                          }
